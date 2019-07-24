5966518cfcbf3ce40e77c1ee961f532888d70c5a
Adnan <adnan.chowdhury@outlook.com>
2019-07-24 15:01:39 -0500

    Set up Form to reflect useState

--- team-builder/src/component/Form.js
+++ team-builder/src/component/Form.js
@@ -5,12 +5,16 @@ const Form = () => {
 
         <form>
             <label>
-                Username:
+                Name:
       <input type="text" id="name" name="name" />
             </label>
             <label>
-                Motto:
-      <input type="text" id="name" name="motto" />
+                E-Mail:
+      <input type="email" id="name" name="email" />
+            </label>
+            <label>
+                Role:
+      <input type="text" id="name" name="role" />
             </label>
             <button type="submit">Submit!</button>
         </form>
