dd2cf83df44f08ee78f2dfdf933405e0b0639d1c
Adnan <adnan.chowdhury@outlook.com>
2019-07-24 14:54:20 -0500

    Added UseState to store form data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Form from './component/Form'
 
 function App() {
+  const [teamUser, setTeamUser]=useState({name:"",email:"",role:""})
+
   return (
     <div className="App">
-      <Form/>
+      <Form />
     </div>
   );
 }
